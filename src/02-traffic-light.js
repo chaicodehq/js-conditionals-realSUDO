e111132f7479659884cf65f5fdb26d07ae99d676
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-02-08 02:20:35 +0530

    Traffic light

--- src/02-traffic-light.js
+++ src/02-traffic-light.js
@@ -22,5 +22,18 @@
  * @returns {string} The driving action to take
  */
 export function getTrafficAction(color) {
-  // Your code here
+	color = color.toLowerCase();
+
+	switch (color) {
+		case "green":
+			return "GO";
+		case "yellow":
+			return "SLOW DOWN";
+		case "red":
+			return "STOP";
+		case "flashing red":
+			return "STOP AND PROCEED WITH CAUTION";
+		default:
+			return "INVALID SIGNAL";
+	}
 }
